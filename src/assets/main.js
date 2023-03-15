e228473dd6983264577dfb3dcb055b114fc649e5
Federico Gomez <gomez00federico@gmail.com>
2023-03-15 11:06:15 -0300

    first commit: structure ready

---
+++ src/assets/main.js
@@ -0,0 +1,10 @@
+const API = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5';
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9e44d9c9admsh727e952373076e5p11b112jsn151bd1e26901',
+		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+	}
+};
+
