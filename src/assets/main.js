0b5917e960d8eaac2871032f4de1dd86db65aac3
Federico Gomez <gomez00federico@gmail.com>
2023-03-15 15:45:04 -0300

    rendered weather

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,6 @@
-const API = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5';
+const API = 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=-31.421877&lon=-64.187962&units=metric&lang=es';
+
+const card_box = null || document.getElementById('card-box');
 
 const options = {
 	method: 'GET',
@@ -8,3 +10,31 @@ const options = {
 	}
 };
 
+async function fetchData(urlApi, options) {
+    const response = await fetch(urlApi, options);
+    const data = response.json();
+
+    return data;
+}
+
+(async () => {
+    try {
+        const weather = await fetchData(API, options);
+        const card = `
+        ${weather.data.map(day => `
+            <section class="day-card">
+                <h2 class="day-card__title">${day.datetime}</h6>
+                <p class="day-card__desc">${day.weather.description}</p>
+                <figure>
+                    <img src="../img/${day.weather.code}.png" alt="Imagen de referencia del clima">
+                </figure>
+                <p class="day-card__temp">${day.temp}°C</p>
+            </section>
+        `).join('')}
+        `;
+
+        card_box.innerHTML = card;
+    } catch (error) {
+        console.error(`Error al renderizar el clima: ${error}`);
+    }
+})()
